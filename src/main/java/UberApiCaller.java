1827e7f0db171d71b7e6d5e0ca96033c831315a0
Daniel Park <Daniel@Daniels-MacBook-Pro.local>
2016-09-29 01:31:07 -0700

    added logic to parse through json object

--- src/main/java/UberApiCaller.java
+++ src/main/java/UberApiCaller.java
@@ -1,4 +1,9 @@
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.Maps;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,13 +11,14 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Map;
 
 public class UberApiCaller {
 
-    private static final double EMERYVILLE_LATITUDE = 0;
-    private static final double EMERYVILLE_LONGITUDE = 0;
-    private static final double SAN_FRANCISCO_LATITUDE = 0;
-    private static final double SAN_FRANCISCO_LONGITUDE = 0;
+    private static final double EMERYVILLE_LATITUDE = 37.846390;
+    private static final double EMERYVILLE_LONGITUDE = -122.293033;
+    private static final double SAN_FRANCISCO_LATITUDE = 37.796860;
+    private static final double SAN_FRANCISCO_LONGITUDE = -122.398786;
     private static final String SERVER_TOKEN = "MfvqqHIflPDOx32PAuxajcNjhdkmBEbHL2gdG0uT";
     private static final String UBER_API_URL = String.format(
             "https://api.uber.com/v1/estimates/price?start_latitude=%s&start_longitude=%s&end_latitude=%s&end_longitude=%s&server_token=%s",
@@ -24,7 +30,8 @@ public class UberApiCaller {
 
     public static void main(String[] args) throws IOException {
         InputStream inputStreamResponse = getResponseFromUber();
-        String xmlResponse = translateToString(inputStreamResponse);
+        String jsonResponse = translateToString(inputStreamResponse);
+        Map<String, Double> uberTypeToPrices = getPricesFromJSON(jsonResponse);
     }
 
     @VisibleForTesting
@@ -43,6 +50,25 @@ public class UberApiCaller {
         return new BufferedReader(new InputStreamReader(inputStreamResponse)).readLine();
     }
 
-
+    @VisibleForTesting
+    static Map<String, Double> getPricesFromJSON(String jsonResponse) {
+        Map<String, Double> typeToPrices = Maps.newHashMap();
+        JsonElement jsonElement = new JsonParser().parse(jsonResponse);
+        JsonObject jsonObject = jsonElement.getAsJsonObject().getAsJsonObject();
+        JsonArray jsonArray = jsonObject.getAsJsonArray("prices");
+        for (int i = 0; i < jsonArray.size(); i++) {
+            JsonObject object = jsonArray.get(i).getAsJsonObject();
+            String uberType = object.get("display_name").getAsString();
+            String priceAsString = object.get("estimate").getAsString();
+            Double price;
+            if (priceAsString.contains("-")) {
+                price = Double.valueOf(priceAsString.substring(priceAsString.indexOf("-") + 1, priceAsString.length()));
+            } else {
+                price = Double.valueOf(priceAsString.substring(1));
+            }
+            typeToPrices.put(uberType, price);
+        }
+        return typeToPrices;
+    }
 
 }
